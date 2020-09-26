fc0a0f06c585e36d971b4b60c3fe04d25d20c2cf
Andres Castillo Quesada <>
2020-09-25 18:15:18 -0600

    final changes to first version based on forked repo

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,15 @@
+// import 'babel-polyfill'
 import { hello, tes } from './globals/import-example'
 
 import '../scss/styles.scss'
+
+// Include css for webpack (for development only)
+// const css = require('../css/main.scss') // eslint-disable-line no-unused-vars
+
+// Hot reloading (for development only)
+// if (module.hot) {
+//   module.hot.accept()
+// }
 
 hello()
 
